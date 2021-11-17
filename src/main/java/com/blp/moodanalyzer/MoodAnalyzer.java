ab69dc1321f3543eed6ad73a8be96774e27844d1
Asus <dhanashreehakke7@gmail.com>
2021-11-17 17:33:15 +0530

    Added: Analyze mood file is added

--- src/main/java/com/blp/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/blp/moodanalyzer/MoodAnalyzer.java
@@ -5,4 +5,23 @@ public class MoodAnalyzer {
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyzer Program");
     }
+
+    /**
+     * Call method to test the mood
+     */
+    public static String analyzeMood(String message) {
+        String mood = null;
+        if (message.contains("sad")) {
+            mood = "SAD";
+        }
+        return mood;
+    }
+
+    public static String analyzeMood1(String message) {
+        String mood = null;
+        if (message.contains("Any")) {
+            mood = "HAPPY";
+        }
+        return mood;
+    }
 }
